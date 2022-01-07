ff9dd3eb79261566e8fd0cb5efdea5be5d0a6da9
Mattia Cardile <mattiaca003@gmail.com>
2022-01-07 23:29:01 +0100

    Removed unused import

--- src/main/java/io/github/mightycoderx/quiteusefulcommands/commands/CustomMapCommand.java
+++ src/main/java/io/github/mightycoderx/quiteusefulcommands/commands/CustomMapCommand.java
@@ -25,7 +25,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
